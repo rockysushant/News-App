80b9ad0126660543b84fdecf59c189e4b3390527
rockysushant <sushantsoni018@gmail.com>
2023-05-26 22:28:55 +0530

    SSS

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Article.kt
@@ -1,6 +1,19 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import  com.google.gson.annotations.SerializedName
+
+
+@Entity(
+    tableName = "articles"
+)
+
+
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+
+    var id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/api/NewsAPI.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/api/NewsAPI.kt
@@ -1,13 +1,12 @@
 package com.androiddevs.mvvmnewsapp.ui.api
 
+import com.androiddevs.mvvmnewsapp.ui.NewsResponse
 import com.androiddevs.mvvmnewsapp.ui.util.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsAPI {
-
-
-
     @GET("v2/top-headlines")
     suspend fun getBreakingNews(
 
@@ -18,5 +17,19 @@ interface NewsAPI {
         @Query("apiKey")
         apiKey:String = API_KEY
 
-    )
+    ): Response<NewsResponse>
+
+
+
+    @GET("v2/top-everything")
+    suspend fun searchForNews(
+
+        @Query("q")
+        searchQuery:String,
+        @Query("page")
+        pageNumber:Int = 1,
+        @Query("apiKey")
+        apiKey:String = API_KEY
+
+    ): Response<NewsResponse>
 }
