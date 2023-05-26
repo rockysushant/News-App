b097ece37e9d473c96a44996209e498f67d7d09a
rockysushant <sushantsoni018@gmail.com>
2023-05-26 13:16:11 +0530

    hii

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Fragments/SearchNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.Fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
+
+}
